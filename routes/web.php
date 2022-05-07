c0c0a254c78f0024d8e3d4b99e75e3950db0e93a
wkndavid <workingnown@gmail.com>
2022-05-07 20:46:48 -0300

    Rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::match(['get', 'post'], '/users', function () {
+Route::get('users', function () {
     return 'h3lloWorld!';
-});
+})->name('users');
+
