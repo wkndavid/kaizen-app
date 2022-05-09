f9a3b3c20ee5dd64b8c413f4894717a6b46cd14b
wkndavid <workingnown@gmail.com>
2022-05-09 08:35:40 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('rota-a', function(){
+    // lógica cabulosa
+    Route::route();
+    return redirect()->route('rotab');
+});
+
+
+Route::get('rota-b', function(){
+    return 'ROTA B';
+})->name('rotab');
+
+
 Route::get('users', function () {
     return 'h3lloWorld!';
 })->name('users');
