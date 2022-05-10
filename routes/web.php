5ba13178f49c0e8ad9fde2ffcb362c62b4c7a553
wkndavid <workingnown@gmail.com>
2022-05-10 11:56:27 -0300

    Rotas de vizualização

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 
+// Route::redirect('rota-a', 'rota-b', 301);
+// Route::permanentRedirect('rota-a', 'rota-b');
+
 Route::get('rota-a', function(){
     // lógica cabulosa
-    Route::route();
     return redirect()->route('rotab');
 });
-
 
 Route::get('rota-b', function(){
     return 'ROTA B';
 })->name('rotab');
 
+Route::view('/welcome', 'welcome', [
+    'title' => 'h3lloWorld kailane!',
+]);
 
 Route::get('users', function () {
     return 'h3lloWorld!';
 })->name('users');
-
