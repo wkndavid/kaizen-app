ef676670de0a1fa428a732925a476c2a1b16922c
wkndavid <workingnown@gmail.com>
2022-05-10 13:18:40 -0300

    Rotas com parâmetros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel 9</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -22,8 +22,6 @@
     </head>
     <body class="antialiased">
 
-    {{ $title }}
-    
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,27 @@ use Illuminate\Support\Facades\Route;
 // Route::redirect('rota-a', 'rota-b', 301);
 // Route::permanentRedirect('rota-a', 'rota-b');
 
-Route::get('rota-a', function(){
-    // lógica cabulosa
-    return redirect()->route('rotab');
+// Route::get('rota-a', function(){
+//     // lógica cabulosa
+//     return redirect()->route('rotab');
+// });
+
+// Route::get('rota-b', function(){
+//     return 'ROTA B';
+// })->name('rotab');
+
+// Route::view('/welcome', 'welcome', [
+//     'title' => 'h3lloWorld kailane!',
+// ]);
+
+// Route::get('users', function () {
+//     return 'h3lloWorld!';
+// })->name('users');
+
+Route::get('/welcome', function () {
+    return view('welcome');
 });
 
-Route::get('rota-b', function(){
-    return 'ROTA B';
-})->name('rotab');
-
-Route::view('/welcome', 'welcome', [
-    'title' => 'h3lloWorld kailane!',
-]);
-
-Route::get('users', function () {
-    return 'h3lloWorld!';
-})->name('users');
+Route::get('/user/{id}/{name}', function($id = null, $name = null){
+    return 'User ' . $id . ' - ' . $name;
+});
